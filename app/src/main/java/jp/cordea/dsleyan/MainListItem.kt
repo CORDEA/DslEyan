8bae7dffc017b690b7f0dea2362390435c2e0b63
Yoshihiro Tanaka <contact@cordea.jp>
2018-04-26 21:15:09 +0900

    add items

---
+++ app/src/main/java/jp/cordea/dsleyan/MainListItem.kt
@@ -0,0 +1,13 @@
+package jp.cordea.dsleyan
+
+import com.xwray.groupie.databinding.BindableItem
+import jp.cordea.dsleyan.databinding.ListItemMainBinding
+
+class MainListItem(private val item: MainItem) : BindableItem<ListItemMainBinding>() {
+
+    override fun getLayout(): Int =
+            R.layout.list_item_main
+
+    override fun bind(viewBinding: ListItemMainBinding, position: Int) {
+    }
+}
---
+++ app/src/main/java/jp/cordea/dsleyan/MainListSection.kt
@@ -0,0 +1,15 @@
+package jp.cordea.dsleyan
+
+import com.xwray.groupie.databinding.BindableItem
+import jp.cordea.dsleyan.databinding.ListItemSectionMainBinding
+
+class MainListSection(
+        private val item: MainListSection
+) : BindableItem<ListItemSectionMainBinding>() {
+
+    override fun getLayout(): Int =
+            R.layout.list_item_section_main
+
+    override fun bind(viewBinding: ListItemSectionMainBinding, position: Int) {
+    }
+}
